3b608d075ba1ac414bcac67398b6fff658018953
Beau Shinkle <beaushinkle@gmail.com>
2015-12-22 13:40:09 -0500

    updated script to use bing api

--- examples/hubot.js
+++ examples/hubot.js
@@ -1,26 +1,21 @@
-/*
-	this bot is a ping pong bot, and every time a message
-	beginning with "ping" is sent, it will reply with
-	"pong".
-*/
-
 var Discord = require("../");
 var client = require('google-images')
 
+var Bing = require('node-bing-api')({accKey: "YaKuygAMWGb+i1kcLewlrJiMluePvD11iPncscIlnx4"});
 // Get the email and password
 var AuthDetails = require("./auth.json");
 
 var bot = new Discord.Client();
 
 bot.on("ready", function () {
-	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+  console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
 });
 
 bot.on("disconnected", function () {
 
-	console.log("Disconnected!");
-	process.exit(1); //exit node.js with an error
-	
+  console.log("Disconnected!");
+  process.exit(1); //exit node.js with an error
+
 });
 
 Array.prototype.chooseRandom = function() {
@@ -29,20 +24,15 @@ Array.prototype.chooseRandom = function() {
 
 bot.on("message", function (msg) {
   var content = msg.content
-	if (content.substring(0, 9) === "image me ") {
+  if (content.substring(0, 9) === "image me ") {
     var query = content.substring(9);
     console.log("received: " + query)
-    client.search(query, function(err, images) {
-      var response = ""
-      if (images.length > 0) {
-        response = images.chooseRandom().unescapedUrl
-      } else {
-        response = "no images for" + query
-      }
-      console.log("response: " + response)
-      bot.sendMessage(msg, response);
-    })
-	}
+    Bing.images(query, {skip: 50}, function(error, res, body){
+      var results = body['d']['results'];
+      var response = results.chooseRandom()['MediaUrl'];
+      bot.sendMessage(msg, response)
+    });
+  }
 
   if (content.substring(0, 6) === "gg me ") {
     var response = "http://champion.gg/champion/" + content.substring(6);
